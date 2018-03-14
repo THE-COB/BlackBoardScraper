c83030ee1d054a6c73e6918ea6a6d6c34826d5a0
THE-COB <jmlife6@gmail.com>
2018-03-13 22:53:37 -0400

    Added a whole lot of shit and pushed an update, productive day

--- content.js
+++ content.js
@@ -1,39 +1,24 @@
-/*
-function onWindowLoad() {
-
-  chrome.tabs.executeScript(null, {
-    file: "unweighted.js"
-  });
-
-}
-
-window.onload = onWindowLoad;
-*/
-
 document.addEventListener("DOMContentLoaded", function() {
 	document.getElementById("weightedButton").addEventListener('click', weightedFunction);
 	document.getElementById("unweightedButton").addEventListener('click', unweightedFunction);
 });
 
-/*
-document.addEventListener("DOMContentLoaded", function() {
-	document.getElementById("unweightedButton").addEventListener('click', weightedFunction);
-});
-
-function getData(){
-	var realDate = document.getElementById("unweightedDate").value;
-	alert(realDate);
-}
-*/
-
 function weightedFunction(){
+	var quarter = document.getElementById("quarter").value;
+	
 	chrome.tabs.executeScript(null, {
-		file: "weighted.js"
-	});	
+		code: "var quarter = "+quarter+"; var weighted = true;"
+	}, function() {
+		chrome.tabs.executeScript(null, {file: 'newCalc.js'});
+	});
 }
-
 function unweightedFunction(){
+	var quarter = document.getElementById("quarter").value;
+	alert(quarter);
+
 	chrome.tabs.executeScript(null, {
-		file: "newCalc.js"
+		code: "var quarter = "+quarter+"; var weighted = false;"
+	}, function() {
+		chrome.tabs.executeScript(null, {file: 'newCalc.js'});
 	});
 }
--- newCalc.js
+++ newCalc.js
@@ -1,6 +1,5 @@
-var weighted = false;
-var quarter = 3;
-
+/*var weighted = false;
+var quarter = 3;*/
 var Grade = class{
 	constructor(gradeEl, isEl){
 		var strScore;
@@ -19,11 +18,11 @@ var Grade = class{
 		}
 		else if(splitScore[0] == "Missing"){
 			gotten = 0;
-			outOf = parseInt(strScore.split("/")[1]);
+			outOf = parseFloat(strScore.split("/")[1]);
 		}
 		else{
-			gotten = parseInt(strScore.split("/")[0]);
-			outOf = parseInt(strScore.split("/")[1]);
+			gotten = parseFloat(strScore.split("/")[0]);
+			outOf = parseFloat(strScore.split("/")[1]);
 		}
 		this.score = [gotten, outOf];
 		this.type = "";
@@ -49,6 +48,15 @@ var Grade = class{
 	}
 }
 
+function searchArr(arr, el){
+	for(var i = 0; i<arr.length; i++){
+		if(arr[i] == el){
+			return i;
+		}
+	}
+	return -1;
+}
+
 var currentQ = document.getElementById("term-"+quarter+"Q");
 var currentGradeStr = currentQ.children[1].innerHTML;
 var currentGrade = parseFloat(currentGradeStr.substring(13, currentGradeStr.length-2));
@@ -57,6 +65,13 @@ var gradeEls = currentQ.children[2].children[2].children[1].children;
 var grades = [];
 for(var i = 0; i<gradeEls.length; i++){
 	grades.push(new Grade(gradeEls[i], true));
+}
+var types = [];
+var isFound = false;
+for(var i = 0; i<grades.length; i++){
+	if(searchArr(types, grades[i].getType()) === -1){
+		types.push(grades[i].getType());
+	}
 }
 
 var isDone = false;
@@ -67,27 +82,48 @@ while(!isDone){
 		break;
 	}
 	else{
-		var newGrade = prompt("What grade do you want to add (xx/xx)");
-		alert(newGrade);
+		var newGrade = prompt("What grade do you want to add (XX/XX)");
 		grades.push(new Grade(newGrade, false));
 
 	}
 }
 
-var totGot = 0;
-var totOut = 0;
-for (var i = 0; i < grades.length; i++) {
-	totGot+=grades[i].getGotten();
-	totOut+=grades[i].getOutOf();
+if(!weighted){
+	var totGot = 0;
+	var totOut = 0;
+	for (var i = 0; i < grades.length; i++) {
+		totGot+=grades[i].getGotten();
+		totOut+=grades[i].getOutOf();
+	}
+	var fullGrade = totGot/totOut;
+	var roundGrade = (fullGrade*100);
+	switch((""+currentGrade).length){
+		case 2: roundGrade = Math.round(roundGrade);
+		case 4: roundGrade = Math.round(roundGrade*10)/10;
+		case 5: roundGrade = Math.round(roundGrade*100)/100;
+	}
+	console.log(roundGrade);
+	alert(roundGrade);
 }
-var fullGrade = totGot/totOut;
-var roundGrade = (fullGrade*100);
-console.log(currentGrade);
-console.log((""+currentGrade).length);
-switch((""+currentGrade).length){
-	case 2: roundGrade = Math.round(roundGrade);
-	case 4: roundGrade = Math.round(roundGrade*10)/10;
-	case 5: roundGrade = Math.round(roundGrade*100)/100;
+else{
+	var typeVal = [];
+	for(var i = 0; i<types.length; i++){
+		typeVal.push(parseFloat(prompt("How much is "+types[i]+" weighted (0.XX)")));
+	}
+	var gradeTypesGotten = [];
+	var gradeTypesOutof = [];
+	for (var i = 0; i < types.length; i++) {
+		gradeTypesGotten.push(0);
+		gradeTypesOutof.push(0);
+	}
+	for(var i = 0; i<grades.length; i++){
+		var nextGrade = grades[i];
+		gradeTypesGotten[searchArr(types, grades[i].getType())] += nextGrade.getGotten();
+		gradeTypesOutof[searchArr(types, grades[i].getType())] += nextGrade.getOutOf();
+	}
+	var totalScore = 0;
+	for(var i = 0; i<types.length; i++){
+		totalScore+=(gradeTypesGotten[i]/gradeTypesOutof[i])*typeVal[i];
+	}
+	alert(totalScore);
 }
-console.log(roundGrade);
-alert(roundGrade);
