d3176f43cdef199684e57a8c773477120bdad29b
Cob <adipo12345678@gmail.com>
2022-04-03 15:32:17 -0400

    fixed recalculating only 2nd quarter

--- V2/recalculate.js
+++ V2/recalculate.js
@@ -105,7 +105,7 @@ else{
 			gradeLetter = "A";
 		}
 		var gradeFormed = Math.round(totalGrade*10000)/100;
-		var gradeDisp = document.querySelector("#term-2Q > h3");
+		var gradeDisp = document.querySelector("#term-"+quarter+"Q > h3");
 		gradeDisp.innerHTML = quarter+"Q Grade: "+gradeLetter+" ["+gradeFormed+"%]";
 	}
 	else{
